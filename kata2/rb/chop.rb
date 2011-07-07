20f2dc4f14c84b9dc86119a7ffb0b616f55aa160
Ian Oxley <ijoxley@gmail.com>
2011-07-07 14:08:47 +0100

    Ruby version of kata 2 started

---
+++ kata2/rb/chop.rb
@@ -0,0 +1,16 @@
+# code kata 2: karate chop
+#
+
+# Returns the index of needle in haystack, or -1 if
+# needle cannot be found
+#
+# needle - an integer
+# haystack - a sorted array of integers
+def chop(needle, haystack)
+  if haystack.empty?
+    return -1
+  end
+
+  size = haystack.size
+  puts size
+end
